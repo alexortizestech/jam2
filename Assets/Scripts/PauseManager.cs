155848c60b34a25c21d640bf8252cac54017b92d
alexortizestech <alejandro.ortiz@escuelaestech.es>
2021-07-21 14:14:41 +0200

    Controller

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
+    public KeyCode Pause;
     public GameObject PauseMenu;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +15,7 @@ public class PauseManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKey(Pause))
         {
             Time.timeScale = 0;
             PauseMenu.SetActive(true);
